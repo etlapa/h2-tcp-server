a116604c9c651b84352e02e2d3dc226944e3f5d6
Martin Eliseo Tlapa Davila <martin.tlapa.davila@accenture.com>
2020-08-18 15:02:08 -0500

    Adding param in properties to run sql file

--- src/main/java/mx/edev/h2/console/Bootstrap.java
+++ src/main/java/mx/edev/h2/console/Bootstrap.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -18,23 +19,27 @@ public class Bootstrap {
 
 	private JdbcTemplate jdbcTemplate;
 
+	@Value("${run.embedded.sql:true}")
+	private boolean runEmbeddedSql;
+
 	public Bootstrap(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	@PostConstruct
 	private void initDb() {
-		String sqlStatements[] = { 
-				"drop table employees if exists",
-				"create table employees(id serial,first_name varchar(255),last_name varchar(255))",
-				"insert into employees(first_name, last_name) values('John','Doe')",
-				"insert into employees(first_name, last_name) values('Jane','Doe')" };
+		if (runEmbeddedSql) {
+			String sqlStatements[] = { "drop table employees if exists",
+					"create table employees(id serial,first_name varchar(255),last_name varchar(255))",
+					"insert into employees(first_name, last_name) values('John','Doe')",
+					"insert into employees(first_name, last_name) values('Jane','Doe')" };
 
-		LOGGER.info("Preparing statements...");
-		Arrays.asList(sqlStatements).forEach(sql -> {
-			LOGGER.info("running: [{}]", sql);
-			jdbcTemplate.execute(sql);
-			LOGGER.info("executed");
-		});
+			LOGGER.info("Preparing statements...");
+			Arrays.asList(sqlStatements).forEach(sql -> {
+				LOGGER.info("running: [{}]", sql);
+				jdbcTemplate.execute(sql);
+				LOGGER.info("executed");
+			});
+		}
 	}
 }

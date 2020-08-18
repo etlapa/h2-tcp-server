6b0d5226828c699ccaafe5617dcb0426c2910d8f
Martin Eliseo Tlapa Davila <martin.tlapa.davila@accenture.com>
2020-08-18 14:15:47 -0500

    Removing unnecesary imports

--- src/main/java/mx/edev/h2/console/Config.java
+++ src/main/java/mx/edev/h2/console/Config.java
@@ -7,7 +7,6 @@ import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
